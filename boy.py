5bae518e1ddfc1afaa0b32625e3311a062226762
백아울 <luna8297@naver.com>
2021-11-18 03:10:20 +0900

    공격 스프라이트+무기

--- boy.py
+++ boy.py
@@ -19,7 +19,8 @@ FRAMES_PER_ACTION = 8
 
 
 # Boy Event
-RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP,UP_DOWN, DOWN_DOWN,DOWN_UP,UP_UP,SPACE_DOWN, JUMP_TIMER = range(10)
+RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP,UP_DOWN, DOWN_DOWN,DOWN_UP,UP_UP,SPACE_DOWN, JUMP_TIMER \
+    ,ATTACK_DOWN,ATTACK_UP= range(12)
 
 key_event_table = {
     (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,
@@ -27,12 +28,16 @@ key_event_table = {
     (SDL_KEYDOWN, SDLK_UP): UP_DOWN,
     (SDL_KEYDOWN, SDLK_DOWN): DOWN_DOWN,
     (SDL_KEYDOWN, SDLK_SPACE): SPACE_DOWN,
+    (SDL_KEYDOWN, SDLK_q):ATTACK_DOWN,
 
     (SDL_KEYUP, SDLK_RIGHT): RIGHT_UP,
     (SDL_KEYUP, SDLK_LEFT): LEFT_UP,
     (SDL_KEYUP, SDLK_UP): UP_UP,
     (SDL_KEYUP, SDLK_DOWN): DOWN_UP,
     #(SDL_KEYUP, SDLK_SPACE): SPACE_UP
+    # 공격키 q
+    (SDL_KEYUP, SDLK_q):ATTACK_UP
+
 }
 
 # 가만히 10, 굴리기 10, 뛰기 8
@@ -189,22 +194,111 @@ class JumpState:
         will.jumptimer -= 5
         if will.jumptimer ==0:
             will.add_event(JUMP_TIMER)
-        will.x = clamp(25, will.x, 1600 - 25)
+        will.x = clamp(35, will.x, 1200-35)
 
     def draw(will):
         will.image2.clip_draw(int(will.frame) * 35, will.direction * 35, 35, 35, will.x, will.y)
 
 
+class AttackState:
 
+    def enter(will, event):
+        if event == RIGHT_DOWN:
+            will.now_max_frame = 8
+            will.direction = 3
+        elif event == LEFT_DOWN:
+            will.direction = 2
+            will.now_max_frame = 8
+        elif event == RIGHT_UP:
+            will.now_max_frame = 8#
+        elif event == LEFT_UP:
+            will.now_max_frame = 8#
+        if event == UP_DOWN:
+            will.direction = 0
+            will.now_max_frame = 8
+        elif event == DOWN_DOWN:
+            will.direction = 1
+            will.now_max_frame = 8
+        elif event == UP_UP:
+            will.now_max_frame = 8#
+        elif event == DOWN_UP:
+            will.now_max_frame = 8#
+        will.jumptimer = 1600
+        will.attack_count+=1
+        if will.state == 'short':
+            will.frame = (will.attack_count * 4 + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 16
+        elif will.state == 'big':
+            will.frame = (will.attack_count * 10 + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 40
+
+    def exit(will, event):
+        if will.state == 'short':
+            if will.frame > (will.attack_count+1)*4 %16:
+                will.add_event(ATTACK_UP)
+            else:
+                will.add_event(ATTACK_DOWN)
+        elif will.state == 'big':
+            if will.frame > (will.attack_count + 1) * 10 %40:
+                will.add_event(ATTACK_UP)
+            else:
+                will.add_event(ATTACK_DOWN)
+        #if event == SPACE:
+            #will.depend() // 방패
+
+    def do(will):
+        if will.state =='short':
+            will.frame = (will.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 16
+        elif will.state == 'big':
+            will.frame = (will.frame  + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) %40
+            #will.x += will.velocity * game_framework.frame_time
+
+        will.x = clamp(35, will.x, 1200 - 35)
+        will.jumptimer -=5
+        if will.jumptimer ==0:
+            will.attack_count =0
+
+    def draw(will):
+        if will.state =='short':#4
+            if will.direction ==1:
+                will.short_down[int(will.frame)].clip_draw(0, 0, 35, 35, will.x, will.y)
+                will.short_solder_down[int(will.frame)].clip_draw(0, 0, 60, 60, will.x, will.y)
+            elif will.direction ==2:
+                will.short_left[int(will.frame)].clip_draw(0, 0, 35, 35, will.x, will.y)
+                will.short_solder_left[int(will.frame)].clip_draw(0, 0, 60, 60, will.x, will.y)
+            elif will.direction == 3:
+                will.short_right[int(will.frame)].clip_draw(0 , 0, 35, 35, will.x, will.y)
+                will.short_solder_right[int(will.frame)].clip_draw(0, 0, 60, 60, will.x, will.y)
+            elif will.direction == 0:
+                will.short_up[int(will.frame)].clip_draw(0, 0, 35, 35, will.x, will.y)
+                will.short_solder_up[int(will.frame)].clip_draw(0, 0, 60, 60, will.x, will.y)
+
+        elif will.state == 'big':#10
+            if will.direction == 1:
+                will.long_down[int(will.frame)].clip_draw(0, 0, 35, 35, will.x, will.y)
+                will.long_toxin_down[int(will.frame)].clip_draw(0, 0, 60, 60, will.x, will.y)
+            elif will.direction == 2:
+                will.long_left[int(will.frame)].clip_draw(0, 0, 35, 35, will.x, will.y)
+                will.long_toxin_left[int(will.frame)].clip_draw(0, 0, 60, 60, will.x, will.y)
+            elif will.direction == 3:
+                will.long_right[int(will.frame)].clip_draw(0, 0, 35, 35, will.x, will.y)
+                will.long_toxin_right[int(will.frame)].clip_draw(0, 0, 60, 60, will.x, will.y)
+            elif will.direction == 0:
+                will.long_up[int(will.frame)].clip_draw(0, 0, 35, 35, will.x, will.y)
+                will.long_toxin_up[int(will.frame)].clip_draw(0, 0, 60, 60, will.x, will.y)
+
+        delay(0.1)
 
 
 next_state_table = {
     IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState, RIGHT_DOWN: RunState, LEFT_DOWN: RunState,  SPACE_DOWN: IdleState,
-                UP_UP: RunState, DOWN_UP: RunState, UP_DOWN: RunState, DOWN_DOWN: RunState},
+                UP_UP: RunState, DOWN_UP: RunState, UP_DOWN: RunState, DOWN_DOWN: RunState,ATTACK_DOWN: AttackState, ATTACK_UP: RunState},
     RunState: {RIGHT_UP: IdleState, LEFT_UP: IdleState, LEFT_DOWN: RunState, RIGHT_DOWN: RunState, SPACE_DOWN: JumpState,
-               UP_UP: IdleState, DOWN_UP: IdleState, UP_DOWN: RunState, DOWN_DOWN: RunState},
+               UP_UP: IdleState, DOWN_UP: IdleState, UP_DOWN: RunState, DOWN_DOWN: RunState,ATTACK_DOWN: AttackState, ATTACK_UP: RunState},
     JumpState: {RIGHT_UP: RunState,LEFT_UP: RunState, LEFT_DOWN: RunState, RIGHT_DOWN: RunState,
-                UP_UP: RunState, DOWN_UP: RunState, UP_DOWN: RunState, DOWN_DOWN: RunState,SPACE_DOWN: JumpState,JUMP_TIMER: RunState}
+                UP_UP: RunState, DOWN_UP: RunState, UP_DOWN: RunState, DOWN_DOWN: RunState,SPACE_DOWN: JumpState,JUMP_TIMER: RunState,
+                ATTACK_DOWN: AttackState, ATTACK_UP: RunState},
+    AttackState: {RIGHT_UP: IdleState, LEFT_UP: IdleState, RIGHT_DOWN: RunState, LEFT_DOWN: RunState, SPACE_DOWN: JumpState,
+                UP_UP: IdleState, DOWN_UP: IdleState, UP_DOWN: RunState, DOWN_DOWN: RunState,ATTACK_DOWN: AttackState, ATTACK_UP: RunState}
+
 }
 
 class Will:
@@ -215,6 +309,32 @@ class Will:
         self.image0 = load_image('resource/Will/will animation cycle35.35.png')
         self.image1 = load_image('resource/Will/Will_Idle35.35.png')
         self.image2 = load_image('resource/Will/Will_Roll35.35.png')
+        self.short_up ={};self.short_down ={};self.short_right ={};self.short_left ={};
+        self.short_solder_up = {};self.short_solder_down = {};self.short_solder_right = {};self.short_solder_left = {};
+        self.long_up ={};self.long_down ={};self.long_right ={};self.long_left ={};
+        self.long_toxin_up = {};self.long_toxin_down = {};self.long_toxin_right = {};self.long_toxin_left = {};
+        self.die= load_image('resource/Will/Death of Will_Export33.41.png')
+        self.state = 'big'
+        self.attack_count =0
+        for i in range(0, 40):
+            self.long_up[i] = load_image('resource/Will/bigsword/Will_BigSwordCombo_Animation_up_%d.png' % (i+1))
+            self.long_down[i] = load_image('resource/Will/bigsword/Will_BigSwordCombo_Animation_Down_%d.png' % (i + 1))
+            self.long_right[i] = load_image('resource/Will/bigsword/Will_BigSwordCombo_Animation_Right_%d.png' % (i + 1))
+            self.long_left[i] = load_image('resource/Will/bigsword/Will_BigSwordCombo_Animation_Left_%d.png' % (i + 1))
+            self.long_toxin_up[i] = ('resource/Will/bigsword/toxi/ToxicBigSwordCombo_Main_Up_%d.png' % (i+1))
+            self.long_toxin_down[i] = ('resource/Will/bigsword/toxi/ToxicBigSwordCombo_Main_Down_%d.png' % (i+1))
+            self.long_toxin_right[i] = ('resource/Will/bigsword/toxi/ToxicBigSwordCombo_Main_Right_%d.png' % (i+1))
+            self.long_toxin_left[i] = ('resource/Will/bigsword/toxi/ToxicBigSwordCombo_Main_Left_%d.png' % (i+1))
+        for i in range(0, 18):
+            self.short_up[i] = load_image('resource/Will/shortsword/Will_ShortSwordCombo_Animation_Up_%d.png' % (i+1))
+            self.short_down[i] = load_image('resource/Will/shortsword/Will_ShortSwordCombo_Animation_Down_%d.png' % (i+1))
+            self.short_right[i] = load_image('resource/Will/shortsword/Will_ShortSwordCombo_Animation_Right_%d.png' % (i+1))
+            self.short_left[i] = load_image('resource/Will/shortsword/Will_ShortSwordCombo_Animation_Left_%d.png' % (i+1))
+            self.short_solder_up[i] = load_image('resource/Will/shortsword/solder/SoldierShortSwordCombo_Main_Up_%d.png' % (i+1))
+            self.short_solder_down[i] = load_image('resource/Will/shortsword/solder/SoldierShortSwordCombo_Main_Down_%d.png' % (i+1))
+            self.short_solder_right[i] = load_image('resource/Will/shortsword/solder/SoldierShortSwordCombo_Main_Right_%d.png' % (i+1))
+            self.short_solder_left[i] = load_image('resource/Will/shortsword/solder/SoldierShortSwordCombo_Main_Left_%d.png' % (i+1))
+
         self.font = load_font('ENCR10B.TTF', 16)
         self.dir = 1
         self.velocity_x = 0
