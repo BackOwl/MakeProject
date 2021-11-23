3bff36b35656452875ae95a0a9ec8a0a584a828a
백아울 <luna8297@naver.com>
2021-11-24 01:38:57 +0900

    서버 맵 고치기

--- grass.py
+++ grass.py
@@ -6,15 +6,22 @@ class Grass:
     def __init__(self):
         self.image0 = load_image('resource/background/background_Level0.png')
         self.image1 = load_image('resource/background/backgroundWalls_Level0.png')
+        self.level = 0
 
     def update(self, mx=0, my=0):
         pass
 
     def monster(self):
-        monster1 = enemy.BabySlime()
-        monster2 = enemy.Stone()
-        game_world.add_object(monster1, 2)
-        game_world.add_object(monster2, 1)
+        if self.level ==0:
+            monster1 = enemy.BabySlime()
+            monster2 = enemy.Stone()
+            game_world.add_object(monster1, 2)
+            game_world.add_object(monster2, 1)
+        elif self.level == 1:
+            monster1 = enemy.BabySlime()
+            monster2 = enemy.Stone()
+            game_world.add_object(monster1, 4)
+            game_world.add_object(monster2, 6)
 
 
     def draw(self):
@@ -26,3 +33,19 @@ class Grass:
     def get_bb(self):
         return 0, 0, 0, 0
 
+class Level2:
+    def __init__(self):
+        self.image0 = load_image('resource/background/Dungeon4_Level0_Background.png')
+        self.image1 = load_image('resource/background/backgroundWalls_Level2.png')
+
+    def update(self, mx=0, my=0):
+        pass
+
+    def draw(self):
+        self.image0.clip_draw(0, 0, 640, 360, 600, 300, 1200, 600)
+        # self.image1.draw(600, 340)
+        self.image1.clip_draw(0, 0, 640, 360, 600, 300, 1200, 600)
+
+    # fill here
+    def get_bb(self):
+        return 0, 0, 0, 0
--- main_state.py
+++ main_state.py
@@ -8,7 +8,7 @@ import game_world
 import server
 
 from boy import Will
-from grass import Grass
+from grass import Grass,Level2
 from enemy import BabySlime, Stone
 from object import Hanari,Sword
 from ball import Ball
@@ -37,14 +37,16 @@ def collide(a, b):
 def enter():
     server.will = Will()
     game_world.add_object(server.will, 1)
+    if server.grasslevel ==0:
+        server.grass = Grass()
+        game_world.add_object(server.grass, 0)
+    elif server.grasslevel == 1:
+        server.grass = Level2()
+        game_world.add_object(server.grass, 0)
 
-    server.grass = Grass()
-    game_world.add_object(server.grass, 0)
-
-    server.monsters = [BabySlime() for i in range(5)]+[Stone() for i in range(3)]
+    server.monsters = [BabySlime() for i in range(server.slime_count)]+[Stone() for i in range(server.golem_count)]
     game_world.add_objects(server.monsters, 1)
-
-    server.break_objects = [Hanari() for i in range(5)]+[Sword() for i in range(1)]
+    server.break_objects = [Hanari() for i in range(server.Hanari_count)]+[Sword() for i in range(server.sword_count)]
     game_world.add_objects(server.break_objects, 1)
 
 
